1007b4896e6c42a56640258e63f2f6ec76bb2c5e
Lorenzo Colitti <lorenzo@google.com>
2014-03-26 19:35:41 +0900

    net: ipv6: autoconf routes into per-device tables
    Currently, IPv6 router discovery always puts routes into
    RT6_TABLE_MAIN. This causes problems for connection managers
    that want to support multiple simultaneous network connections
    and want control over which one is used by default (e.g., wifi
    and wired).
    
    To work around this connection managers typically take the routes
    they prefer and copy them to static routes with low metrics in
    the main table. This puts the burden on the connection manager
    to watch netlink to see if the routes have changed, delete the
    routes when their lifetime expires, etc.
    
    Instead, this patch adds a per-interface sysctl to have the
    kernel put autoconf routes into different tables. This allows
    each interface to have its own autoconf table, and choosing the
    default interface (or using different interfaces at the same
    time for different types of traffic) can be done using
    appropriate ip rules.
    
    The sysctl behaves as follows:
    
    - = 0: default. Put routes into RT6_TABLE_MAIN as before.
    - > 0: manual. Put routes into the specified table.
    - < 0: automatic. Add the absolute value of the sysctl to the
           device's ifindex, and use that table.
    
    The automatic mode is most useful in conjunction with
    net.ipv6.conf.default.accept_ra_rt_table. A connection manager
    or distribution could set it to, say, -100 on boot, and
    thereafter just use IP rules.
    
    Change-Id: I093d39fb06ec413905dc0d0d5792c1bc5d5c73a9
    Signed-off-by: Lorenzo Colitti <lorenzo@google.com>
    Git-commit: 5fe5c512af518d0abbbc0d2fafa8e355f518c2a9
    Git-repo: https://android.googlesource.com/kernel/common.git
    [imaund@codeaurora.org: Resolve trivial merge conflicts]
    Signed-off-by: Ian Maund <imaund@codeaurora.org>
    
    Signed-off-by: tarun93 <tarunmyid@gmail.com>
    Signed-off-by: Monish <monishk10@yahoo.com>

--- net/ipv6/route.c
+++ net/ipv6/route.c
@@ -1958,33 +1958,14 @@ struct rt6_info *rt6_add_dflt_router(const struct in6_addr *gwaddr,
 
 int rt6_addrconf_purge(struct rt6_info *rt, void *arg) {
 	if (rt->rt6i_flags & (RTF_DEFAULT | RTF_ADDRCONF) &&
-		(!rt->rt6i_idev || rt->rt6i_idev->cnf.accept_ra != 2))
+	    (!rt->rt6i_idev || rt->rt6i_idev->cnf.accept_ra != 2))
 		return -1;
 	return 0;
 }
 
 void rt6_purge_dflt_routers(struct net *net)
 {
-	struct rt6_info *rt;
-	struct fib6_table *table;
-
-	/* NOTE: Keep consistent with rt6_get_dflt_router */
-	table = fib6_get_table(net, RT6_TABLE_DFLT);
-	if (!table)
-		return;
-
-restart:
-	read_lock_bh(&table->tb6_lock);
-	for (rt = table->tb6_root.leaf; rt; rt = rt->dst.rt6_next) {
-		if (rt->rt6i_flags & (RTF_DEFAULT | RTF_ADDRCONF) &&
-		    (!rt->rt6i_idev || rt->rt6i_idev->cnf.accept_ra != 2)) {
-			dst_hold(&rt->dst);
-			read_unlock_bh(&table->tb6_lock);
-			ip6_del_rt(rt);
-			goto restart;
-		}
-	}
-	read_unlock_bh(&table->tb6_lock);
+	fib6_clean_all(net, rt6_addrconf_purge, 0, NULL);
 }
 
 static void rtmsg_to_fib6_config(struct net *net,
