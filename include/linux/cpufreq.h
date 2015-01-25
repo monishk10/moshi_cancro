290dbc2cd3d4fa7080e8b5dda9cc1e231d0e755c
Pranav Vashi <neobuddy89@gmail.com>
2015-01-26 04:47:13 +0530

    cpufreq: Add yankactive governor
    * Cleanup and tuning by dorimanx.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Monish <monishk10@yahoo.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -386,6 +386,10 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMANDPLUS)
 extern struct cpufreq_governor cpufreq_gov_ondemandplus;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_ondemandplus)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_YANKACTIVE)
+extern struct cpufreq_governor cpufreq_gov_yankactive;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_yankactive)
+>>>>>>> 5b2ff4a... cpufreq: Add yankactive governor
 #endif
 
 
