2e448c7fd073783d4510668fbe9cd1664e4a3896
Pranav Vashi <neobuddy89@gmail.com>
2015-02-09 00:41:52 +0530

    cpufreq: Add Impulse governor
    * Experimental.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Monish <monishk10@yahoo.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -389,7 +389,9 @@ extern struct cpufreq_governor cpufreq_gov_ondemandplus;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_YANKACTIVE)
 extern struct cpufreq_governor cpufreq_gov_yankactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_yankactive)
->>>>>>> 5b2ff4a... cpufreq: Add yankactive governor
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_IMPULSE)
+extern struct cpufreq_governor cpufreq_gov_impulse;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_impulse)
 #endif
 
 
