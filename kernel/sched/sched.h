ae71b4c6c0f55fc92d5941357757cb573370f183
Monish <monishk10@yahoo.com>
2015-11-05 16:56:54 +0530

    Revert "sched/fair: Implement fast idling of CPUs when the system is partially loaded"
    This reverts commit d3e7866aa49be1450d6b4d1b8e3e68cfeb9b1071.

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -328,9 +328,6 @@ struct root_domain {
 	cpumask_var_t span;
 	cpumask_var_t online;
 
-	/* Indicate more than one runnable task for any CPU */
-	bool overload;
-
 	/*
 	 * The "RT overload" flag: it gets set if a CPU has more than
 	 * one runnable RT task.
@@ -975,11 +972,9 @@ static inline void inc_nr_running(struct rq *rq)
 	nr_stats->nr_last_stamp = rq->clock_task;
 #endif
 	rq->nr_running++;
-
-	if (rq->nr_running >= 2) {
-		if (!rq->rd->overload)
-			rq->rd->overload = true;
-	}
+#if defined(CONFIG_MSM_RUN_QUEUE_STATS_BE_CONSERVATIVE)
+	write_seqcount_end(&nr_stats->ave_seqcnt);
+#endif
 }
 
 static inline void dec_nr_running(struct rq *rq)
