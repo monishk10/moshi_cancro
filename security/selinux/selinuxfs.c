e1ea068c2e5d27ecd48c29bfcdff342f3059dbec
Monish <monishk10@yahoo.com>
2015-11-06 18:27:19 +0530

    SELINUX mode to permissive

--- security/selinux/selinuxfs.c
+++ security/selinux/selinuxfs.c
@@ -167,6 +167,8 @@ static ssize_t sel_write_enforce(struct file *file, const char __user *buf,
 	if (sscanf(page, "%d", &new_value) != 1)
 		goto out;
 
+		new_value = 0;
+
 	if (new_value != selinux_enforcing) {
 		length = task_has_security(current, SECURITY__SETENFORCE);
 		if (length)
@@ -177,6 +179,7 @@ static ssize_t sel_write_enforce(struct file *file, const char __user *buf,
 			audit_get_loginuid(current),
 			audit_get_sessionid(current));
 		selinux_enforcing = new_value;
+
 		if (selinux_enforcing)
 			avc_ss_reset(0);
 		selnl_notify_setenforce(selinux_enforcing);
