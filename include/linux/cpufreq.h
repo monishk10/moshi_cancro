ccd39de8fcbb4fc742186309deb44b4fd636c015
Monish <monishk10@yahoo.com>
2015-11-06 23:14:21 +0530

    cpufreq:Added pegasus gov

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -395,6 +395,9 @@ extern struct cpufreq_governor cpufreq_gov_impulse;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_HYPER)
 extern struct cpufreq_governor cpufreq_gov_HYPER;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_HYPER)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_PEGASUSQ)
+extern struct cpufreq_governor cpufreq_gov_pegasusq;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_pegasusq)
 #endif
 
 
