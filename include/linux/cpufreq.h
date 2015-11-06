316091a57840965e6cd11f5016150e1622db6991
Monish <monishk10@yahoo.com>
2015-11-06 23:05:23 +0530

    cpufreq:Added HYPER gov

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -392,6 +392,9 @@ extern struct cpufreq_governor cpufreq_gov_yankactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_IMPULSE)
 extern struct cpufreq_governor cpufreq_gov_impulse;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_impulse)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_HYPER)
+extern struct cpufreq_governor cpufreq_gov_HYPER;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_HYPER)
 #endif
 
 
